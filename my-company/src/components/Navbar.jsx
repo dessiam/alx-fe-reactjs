6be628ba8ed48262620e57a52a856537c1c3550d
Desmond Essiam <desmondessiam03@gmail.com>
2026-01-19 22:37:18 +0000

    Add new app project

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{
+      backgroundColor: '#222',
+      padding: '10px',
+      display: 'flex',
+      gap: '15px'
+    }}>
+      <Link style={{ color: 'white' }} to="/">Home</Link>
+      <Link style={{ color: 'white' }} to="/about">About</Link>
+      <Link style={{ color: 'white' }} to="/services">Services</Link>
+      <Link style={{ color: 'white' }} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
