48b0e2f6a28e408ac6fabb690701f811a4d06022
Desmond Essiam <desmondessiam03@gmail.com>
2026-01-29 03:49:25 +0000

    Added deleate button with other components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Recipe Sharing App</h1>
-
+    <BrowserRouter>
       <AddRecipeForm />
-      <RecipeList />
-    </div>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,13 @@
+import { useRecipeStore } from '../recipeStore';
+
+function DeleteRecipeButton({ id }) {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+
+  return (
+    <button onClick={() => deleteRecipe(id)} style={{ marginTop: '10px', color: 'red' }}>
+      Delete Recipe
+    </button>
+  );
+}
+
+export default DeleteRecipeButton;
