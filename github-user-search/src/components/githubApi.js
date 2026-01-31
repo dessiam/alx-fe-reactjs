ae55aba80e85647e151eeb58347857f0dab31dd2
Desmond Essiam <desmondessiam03@gmail.com>
2026-01-31 21:56:42 +0000

    New search app in react

---
+++ github-user-search/src/components/githubApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const githubApi = axios.create({
+  baseURL: "https://api.github.com",
+  headers: {
+    Accept: "application/vnd.github+json",
+    Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+      ? `Bearer ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+      : undefined,
+  },
+});
+
+export default githubApi;
