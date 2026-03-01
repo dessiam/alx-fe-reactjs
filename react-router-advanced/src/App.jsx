6b7e27ce0a1d8f74916382e13c8e9aaaa8ac89f2
Desmond Essiam <desmondessiam03@gmail.com>
2026-03-01 14:40:33 +0000

    flag fix

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,45 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Profile from "./components/Profile";
-import ProfileDetails from "./components/ProfileDetails";
-import ProfileSettings from "./components/ProfileSettings";
 import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-
     <BrowserRouter>
 
       <Routes>
 
         <Route path="/" element={<h1>Home</h1>} />
 
-        {/* Protected Profile Route */}
         <Route
-          path="/profile"
+          path="/profile/*"
           element={
             <ProtectedRoute>
               <Profile />
             </ProtectedRoute>
           }
-        >
-
-          {/* Nested Routes */}
-
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-
-        </Route>
-
-        {/* Dynamic Route */}
+        />
 
         <Route path="/post/:id" element={<BlogPost />} />
 
       </Routes>
 
     </BrowserRouter>
-
   );
 }
 
--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,4 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 function Profile() {
   return (
@@ -7,11 +9,19 @@ function Profile() {
       <h2>Profile Page</h2>
 
       <nav>
-        <Link to="details">Profile Details</Link> |{" "}
-        <Link to="settings">Profile Settings</Link>
+        <Link to="details">Details</Link> |{" "}
+        <Link to="settings">Settings</Link>
       </nav>
 
-      <Outlet />
+      {/* Nested Routes */}
+
+      <Routes>
+
+        <Route path="details" element={<ProfileDetails />} />
+
+        <Route path="settings" element={<ProfileSettings />} />
+
+      </Routes>
 
     </div>
   );
